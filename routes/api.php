2bc2cf59d47afd8e11bc67611fe609fade9f4383
Dário Junior <dariojunior04@gmail.com>
2021-02-20 13:33:41 -0300

    Implementando rotas Pets

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('pets', PetController::class)->only([
+    'index', 'show', 'store', 'destroy'
+]);
