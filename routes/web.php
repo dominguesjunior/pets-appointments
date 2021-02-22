698dece81425507bdc447fceb498ecc552c30091
Dário Junior <dariojunior04@gmail.com>
2021-02-21 22:22:45 -0300

    Adicionando documentação

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('documentation');
+});
+
+Route::get('/documentation', function () {
+    return view('documentation');
 });
